909e8ae479cae9e4ae986d9bfa65821e84eabc9c
misraag <jaiofficial50@gmail.com>
2024-07-08 11:50:03 +0530

    outsourcing validtion logic

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Input from "./Input";
+import { isEmail, isNotEmpty, hasMinLength } from "../util/validation";
+
 
 export default function Login() {
   const [enteredValues, setEnteredValues] = useState({
@@ -11,9 +13,9 @@ export default function Login() {
     password: false,
   });
 
-  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
+  const emailIsInvalid = didEdit.email && !isEmail(enteredValues.email) && isNotEmpty(enteredValues.email);
   const passwordIsInvalid =
-    didEdit.password && enteredValues.password.trim().length < 6;
+    didEdit.password && !hasMinLength(enteredValues.password, 6)
 
   function handleSubmit(event) {
     event.preventDefault();
